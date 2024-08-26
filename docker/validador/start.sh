636a35af94db031867ca204aa6607ae0c4e0028e
call-me-roger <reyrogerio@hotmail.com>
2024-08-26 13:06:09 -0300

    chore(docker/validator/start.sh): added SYNC_MODE .env var

--- docker/validador/start.sh
+++ docker/validador/start.sh
@@ -92,14 +92,14 @@ if [ $TYPE_BLOCKCHAIN == "VALIDATOR" ]; then
     fi
 
     # Run Validator Node
-    ./utils/geth --datadir=./ --config ./config.toml --syncmode 'full' \
+    ./utils/geth --datadir=./ --config ./config.toml --syncmode ${SYNC_MODE} --rpc --rpcapi \
     --networkid $CHAIN_ID --nat extip:"$NODEIP" --port "$NODEPORT" \
     --http --http.addr 0.0.0.0 --http.port $NODE_HTTP_PORT --http.api admin,eth,miner,net,txpool,clique,personal,web3,debug \
     --ws --ws.addr 0.0.0.0 --ws.port $NODE_WS_PORT --ws.origins "" --ws.api "web3, net, eth," \
     --allow-insecure-unlock --unlock $WALLET_ACCOUNT --password ./keystore/password.txt \
     --mine --miner.etherbase $WALLET_ACCOUNT
 else
-    ./utils/geth --datadir=./ --config ./config.toml --syncmode 'full' --networkid $CHAIN_ID --nat extip:"$NODEIP" --port "$NODEPORT" \
+    ./utils/geth --datadir=./ --config ./config.toml --syncmode ${SYNC_MODE} --rpc --rpcapi --networkid $CHAIN_ID --nat extip:"$NODEIP" --port "$NODEPORT" \
     --http --http.addr 0.0.0.0 --http.port $NODE_HTTP_PORT --http.api admin,eth,miner,net,txpool,personal,web3,debug \
     --ws --ws.addr 0.0.0.0 --ws.port $NODE_WS_PORT --ws.origins "" --ws.api "web3, net, eth"
 fi
